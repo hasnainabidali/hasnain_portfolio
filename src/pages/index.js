ad3395fefc42cd59110ef96da6ac3de0f8aa6cf7
Hasnainraza110 <hasnainrhraza@gmail.com>
2023-08-29 17:47:03 +0500

    all files

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import { Header } from '@/components';
+import { HomeHero } from '@/Layout';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Head>
+        <title>Juliet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=" w-auto ">
+        <Header />
+        <div className="bg-white">
+          <HomeHero />
+        </div>
+      </main>
+    </div>
+  );
+}
